ddf451830dfa8b968a50ca4d408427b6ad512b43
Marvin Libam <libam.marvin@gmail.com>
2022-11-04 18:27:02 +0100

    feat: navigation: start screen setup

---
+++ app/src/main/java/com/example/kingofsmash/fragment/CharacterSelectionFragment.kt
@@ -0,0 +1,31 @@
+package com.example.kingofsmash.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.kingofsmash.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CharacterSelectionFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CharacterSelectionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_character_selection, container, false)
+        val button = view.findViewById<Button>(R.id.button_navigate_to_main_fragment)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_characterSelectionFragment_to_fragment_main)
+        }
+        return view
+    }
+}
